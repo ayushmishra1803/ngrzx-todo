bd64f8749cbea09348b5e8c4ba32cac77d2c153a
ayushmishra18013 <ayushmishra0810@gmail.com>
2022-02-13 19:27:58 +0530

    Chnages

--- src/app/service/store.service.ts
+++ src/app/service/store.service.ts
@@ -13,6 +13,7 @@ export class StoreService {
     const data: TODO = { title: title, id: initalId, isEdit: false };
     this.store.dispatch(new TODOActions.AddTodo(data));
   }
+  
   deleteTodo(id: number) {
     this.store.dispatch(new TODOActions.DeleteTodo(id));
   }
