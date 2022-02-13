283ebe77fd167e50262ce4763b86f91365520b6f
ayushmishra18013 <ayushmishra0810@gmail.com>
2022-02-13 21:24:36 +0530

    Changes

--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'ngrx-list-item',
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.scss'],
 })
-export class ListItemComponent implements OnInit {
+export class ListItemComponent implements OnInit, OnChanges {
   constructor() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.todo);
+  }
   @Input() todo: any;
 
   ngOnInit(): void {}
