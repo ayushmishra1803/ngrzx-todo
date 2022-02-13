a1c40974d65d9951b4304bc22d92a2dfa6219e89
ayushmishra18013 <ayushmishra0810@gmail.com>
2022-02-13 21:26:40 +0530

    Upda6tes

--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -5,18 +5,18 @@ import {
   OnInit,
   SimpleChanges,
 } from '@angular/core';
+import { StoreService } from 'src/app/service/store.service';
 
 @Component({
   selector: 'ngrx-list-item',
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.scss'],
 })
-export class ListItemComponent implements OnInit, OnChanges {
-  constructor() {}
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(this.todo);
-  }
+export class ListItemComponent implements OnInit {
+  constructor(private storeService: StoreService) {}
+
   @Input() todo: any;
 
   ngOnInit(): void {}
+  deleteTodo(id: number) {}
 }
